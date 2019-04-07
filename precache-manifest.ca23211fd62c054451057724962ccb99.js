db61a4800f89ef974a2afea9c2c35cb82d4fb771
mentosti <mentosti29071994@gmail.com>
2019-04-07 09:24:15 -0400

    Updates

---
+++ precache-manifest.ca23211fd62c054451057724962ccb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-calculator/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "731050bb9090e5e1c1dd",
+    "url": "/react-calculator/static/js/main.e1f14a87.chunk.js"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-calculator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "731050bb9090e5e1c1dd",
+    "url": "/react-calculator/static/css/main.82b75e81.chunk.css"
+  },
+  {
+    "revision": "a032cab73925c7a6f94c0a4d62420268",
+    "url": "/react-calculator/index.html"
+  }
+];
